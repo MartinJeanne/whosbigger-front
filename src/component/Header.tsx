d954e5ea6638c6f16b520aae454f58f640c3a74f
MartinJeanne <martinjeanne.dev@gmail.com>
2024-05-21 00:20:16 +0200

    Improve color bg

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -5,18 +5,18 @@ type HeaderProps = {
 };
 
 export default function Header({ userAwnser }: HeaderProps) {
-    let answerClass = '';
-    if (userAwnser === 'Correct !') {
-        answerClass = 'correct';
-    } else if (userAwnser === 'Wrong') {
-        answerClass = 'wrong';
+    let bgColor = 'white';
+    if (userAwnser === 'Correct ✅') {
+        bgColor = '#85fca9';
+    } else if (userAwnser === 'Wrong ❌') {
+        bgColor = '#fc8585';
     }
 
     return (
-        <header className='header'>
+        <header className='header' style={{background: bgColor}}>
             <div className='question'>Who's bigger?</div>
             <div className='awnser-and-next'>
-                <div className={`user-answer ${answerClass}`}>
+                <div className='user-answer'>
                     {userAwnser}
                 </div>
                 <button className='next-button' onClick={() => location.reload()}>Next</button>
