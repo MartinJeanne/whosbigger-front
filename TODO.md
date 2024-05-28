fa58465aae36bc2a2773587dc308bab24c27ccbc
MartinJeanne <martinjeanne.dev@gmail.com>
2024-05-28 23:23:20 +0200

    map draft!

--- TODO.md
+++ TODO.md
@@ -1,3 +1,2 @@
-handle difficulty in front
 make a map page, let user guess the place of the cities on the map (geokeo)
 leaderboard, with username, save username in cookie
--- src/component/Map.tsx
+++ src/component/Map.tsx
@@ -1,12 +1,45 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { MapContainer, TileLayer, Popup, useMapEvents, CircleMarker } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 export default function Map() {
     const { location } = useParams();
+    console.log(location);
+
+    const position = [49.1846, -0.3722];
+    const greenOptions = { color: 'green' }
 
     return (
-        <div>
-            <div>Hey there</div>
-            <p>Location: {location}</p>
-        </div>
+        <MapContainer
+            center={position}
+            zoom={10}
+            style={{ height: "100vh", width: "100%" }}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <CircleMarker center={position} pathOptions={greenOptions} radius={20}>
+                <Popup>Correct!</Popup>
+            </CircleMarker>
+            <LocationMarker />
+        </MapContainer>
+    );
+}
+
+function LocationMarker() {
+    const [position, setPosition] = useState(null);
+
+    useMapEvents({
+        click(e) {
+            setPosition(e.latlng);
+        },
+    });
+
+    return position === null ? null : (
+        <Popup position={position}>
+            <div>
+                You clicked at <br />
+                Latitude: {position.lat.toFixed(4)}, <br />
+                Longitude: {position.lng.toFixed(4)}
+            </div>
+        </Popup>
     );
 }
